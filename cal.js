db725d9cee6bf6a4decb4e8217554947e9236d17
Akshay More <45824962+theakshaymore@users.noreply.github.com>
2020-08-21 00:07:58 +0530

    calculator

---
+++ cal.js
@@ -0,0 +1,49 @@
+var one = () => {
+  var one = document.querySelector("#one").value;
+  console.log(one);
+};
+var two = () => {
+  var two = document.querySelector("#two").value;
+  console.log(two);
+};
+var three = () => {
+  var three = document.querySelector("#three").value;
+  console.log(three);
+};
+var four = () => {
+  var four = document.querySelector("#four").value;
+  console.log(four);
+};
+var five = () => {
+  var five = document.querySelector("#five").value;
+  console.log(five);
+};
+var six = () => {
+  var six = document.querySelector("#six").value;
+  console.log(six);
+};
+var seven = () => {
+  var seven = document.querySelector("#seven").value;
+  console.log(seven);
+};
+var eight = () => {
+  var eight = document.querySelector("#eight").value;
+  console.log(eight);
+};
+var nine = () => {
+  var nine = document.querySelector("#nine").value;
+  console.log(nine);
+};
+var plus = () => {
+    var plus = document.querySelector("#plus").value;
+    return plus;
+}
+ var rr;
+function res (plus) {
+    if (plus) {
+        return rr = 
+        
+    } else {
+        
+    }
+}
